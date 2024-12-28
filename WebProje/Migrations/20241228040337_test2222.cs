99183659381e3e80e1847385e5075e5fcd2db847
amil1105 <sxiyev4@gmail.com>
2024-12-28 07:04:42 +0300

    Son Düzenlemeler

---
+++ WebProje/Migrations/20241228040337_test2222.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebProje.Migrations
+{
+    public partial class test2222 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
